bd5fcdf12a68ba6be3db7bc685fcbeb559eb9dde
LGustavo2611 <luchogustavodeoliv26@gmail.com>
2025-11-25 17:53:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const computer = document.querySelector(".computer img");
+const player = document.querySelector(".player img");
+const computerPoints = document.querySelector(".computerPoints");
+const playerPoints = document.querySelector(".playerPoints");
+const options = document.querySelectorAll(".options button");
+const bgMusic = document.getElementById("bgMusic");
+const sfxMap = {
+  STONE: "./song/stone.mp3",
+  PAPER: "./song/paper.mp3",
+  SCISSORS: "./song/scissors.mp3",
+};
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    bgMusic.play().catch((e) => console.log("Música de fundo bloqueada."));
+
+    const chosenOption = option.innerHTML;
+    if (sfxMap[chosenOption]) {
+      const sound = new Audio(sfxMap[chosenOption]);
+      sound.play();
+    }
+
+    computer.classList.add("shakeComputer");
+    player.classList.add("shakePlayer");
+
+    setTimeout(() => {
+      computer.classList.remove("shakeComputer");
+      player.classList.remove("shakePlayer");
+
+      player.src = "./src/" + option.innerHTML + "Player.png";
+
+      const choice = ["STONE", "PAPER", "SCISSORS"];
+      let arrayNo = Math.floor(Math.random() * 3);
+      let computerChoice = choice[arrayNo];
+      
+      computer.src = "./src/" + computerChoice + "Computer.png";
+
+      let cPoints = parseInt(computerPoints.innerHTML);
+      let pPoints = parseInt(playerPoints.innerHTML);
+
+      if (option.innerHTML === "STONE") {
+        if (computerChoice === "PAPER") computerPoints.innerHTML = cPoints + 1;
+        else if (computerChoice === "SCISSORS")
+          playerPoints.innerHTML = pPoints + 1;
+      } else if (option.innerHTML === "PAPER") {
+        if (computerChoice === "SCISSORS")
+          computerPoints.innerHTML = cPoints + 1;
+        else if (computerChoice === "STONE")
+          playerPoints.innerHTML = pPoints + 1;
+      } else {
+        if (computerChoice === "STONE") computerPoints.innerHTML = cPoints + 1;
+        else if (computerChoice === "PAPER")
+          playerPoints.innerHTML = pPoints + 1;
+      }
+    }, 900);
+  });
+});
